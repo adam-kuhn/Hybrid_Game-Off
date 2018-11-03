b9a057184f008161e7707b8ebe469444a182c4d2
adam-kuhn <adam.kuhn20@gmail.com>
2018-11-03 09:03:11 +0200

    playing with physics

---
+++ Assets/Player1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb.useGravity = false;
+	}
+	
+	// Update is called once per frame
+    // use FixedUpdate for programming physics
+	void FixedUpdate () {
+        rb.AddForce(0, 20 * Time.deltaTime, 200 * Time.deltaTime);
+	}
+}
