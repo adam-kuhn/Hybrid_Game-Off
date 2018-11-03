4d13036baa1ed03e7b07caab44cb1d33d2e87db2
adam-kuhn <adam.kuhn20@gmail.com>
2018-11-03 20:08:41 +0200

    adjust controls

--- Assets/Player1.cs
+++ Assets/Player1.cs
@@ -5,46 +5,85 @@ using UnityEngine;
 public class Player1 : MonoBehaviour {
 
     public Rigidbody rb;
-    //float is a number with decimals
+    //float is a number with decimals "f" after number means float
     public float forwardForce = 200f;
+    public float sidewaysMovement = 1200;
+    public float forwardMovement = 1500;
+    public float backwardMovement = -1000f;
+
+    public bool moveRight = false;
+    public bool moveLeft = false;
+    public bool moveUp = false;
+    public bool moveBack = false;
+
 
     // Use this for initialization
     void Start () 
     {
-        rb.useGravity = false;
-	}
-	
-	// Update is called once per frame
+   
+    }
+
+    void Update()
+    {
+        // below is sames as 
+        // if (Input.GetKey("d") || Input.GetKey("right")) { moveRight = true }
+        // | will only compute false if both values are false
+        // so if moveRight is false and getKey is true ==> true will be retured and a move occurs
+        moveRight |= Input.GetKey("d") || Input.GetKey("right");
+        // below is same as
+        // if (Input.GetKeyUp("d") || Inpute.GetKeyUp("right") { moveRight = false }
+        // & will onle compute true if both values are true
+        // so if moveRight is true and key up is false (one key up will be false the other will be true), moveRight is set to false and move stops
+        moveRight &= !Input.GetKeyUp("d") && !Input.GetKeyUp("right");
+
+        moveLeft |= Input.GetKey("a") || Input.GetKey("left");
+        moveLeft &= !Input.GetKeyUp("a") && !Input.GetKeyUp("left");
+        //if (Input.GetKey("a") || Input.GetKey("left")) 
+        //{
+        //    moveLeft = true;
+
+        //}
+        //if (Input.GetKeyUp("a") || Input.GetKeyUp("left"))
+        //{
+        //    moveLeft = false;
+        //}
+        moveUp |= Input.GetKey("w") || Input.GetKey("up");
+        moveUp &= !Input.GetKeyUp("w") && !Input.GetKeyUp("up");
+
+        moveBack |= Input.GetKey("s") || Input.GetKey("down");
+        moveBack &= !Input.GetKeyUp("s") && !Input.GetKeyUp("down");
+    }
+
+    // Update is called once per frame
     // use FixedUpdate for programming physics
-	void FixedUpdate () 
+    void FixedUpdate () 
     {
         // inputs go x, y, z
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-        if (Input.GetKey("d") || Input.GetKey("right")) 
+        if (moveRight) 
         {
-            rb.AddForce(1000 * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysMovement * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("a") || Input.GetKey("left"))
+        if (moveLeft)
         {
-            rb.AddForce(-1000 * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysMovement * Time.deltaTime, 0, 0);
         }
 
-        if (Input.GetKey("s") || Input.GetKey("down")) 
+        if (moveBack) 
         {
-            rb.AddForce(0, 0, -1000 * Time.deltaTime);
+            rb.AddForce(0, 0, backwardMovement * Time.deltaTime);
         }
 
-        if (Input.GetKey("w") || Input.GetKey("up")) 
+        if (moveUp) 
         {
-            rb.AddForce(0, 0, 1000 * Time.deltaTime);
+            rb.AddForce(0, 0, forwardMovement * Time.deltaTime);
         }
 	}
 
     void OnCollisionEnter() 
     {
         Debug.Log("I've been hit");
-        rb.useGravity = true;
     }
 }
