a6ce6843182ba9c9f79d872e81dc7acf8c4be5aa
adam-kuhn <adam.kuhn20@gmail.com>
2018-11-03 18:54:15 +0200

    able to read direction key inputs

--- Assets/Player1.cs
+++ Assets/Player1.cs
@@ -5,19 +5,45 @@ using UnityEngine;
 public class Player1 : MonoBehaviour {
 
     public Rigidbody rb;
+    //float is a number with decimals
+    public float forwardForce = 200f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () 
+    {
         rb.useGravity = false;
 	}
 	
 	// Update is called once per frame
     // use FixedUpdate for programming physics
-	void FixedUpdate () {
-        rb.AddForce(0, 20 * Time.deltaTime, 200 * Time.deltaTime);
+	void FixedUpdate () 
+    {
+        // inputs go x, y, z
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") || Input.GetKey("right")) 
+        {
+            Debug.Log("RIGHT direction");
+        }
+
+        if (Input.GetKey("s") || Input.GetKey("down")) 
+        {
+            Debug.Log("DOWN direction");
+        }
+
+        if (Input.GetKey("a") || Input.GetKey("left")) 
+        {
+            Debug.Log("LEFT direction");
+        }
+
+        if (Input.GetKey("w") || Input.GetKey("up")) 
+        {
+            Debug.Log("FORWARD please");
+        }
 	}
 
-    void OnCollisionEnter() {
+    void OnCollisionEnter() 
+    {
         Debug.Log("I've been hit");
         rb.useGravity = true;
     }
---
+++ Assets/ground_phys.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ground_phys : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(0, 20 * Time.deltaTime, 200 * Time.deltaTime);
+    }
+}
